04119263f5a95573ed3a939e2644d882ccfd3bdd
Richard Bailey <richard@richardbailey.xyz>
2020-04-20 15:19:27 +0100

    Create pixelated image. Works for simple blocky drawings

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ import (
 	"github.com/disintegration/imaging"
 )
 
+// Define the dimentions of the pieces
 const (
-	pieceWidth  = 1
-	pieceHeight = 1
+	pieceWidth  = 10
+	pieceHeight = 10
 )
 
 type piece struct {
@@ -22,47 +23,54 @@ type piece struct {
 
 func main() {
 	// Load the source image
-	src, _ := imaging.Open("images/sky.jpg")
+	src, _ := imaging.Open("images/monkey.jpg")
 
 	// Determine new dimentions to be a multiple of the respective piece dimentions
+	// Integer division helps here
 	newx := (src.Bounds().Max.X / pieceWidth) * pieceWidth
 	newy := (src.Bounds().Max.Y / pieceHeight) * pieceHeight
 
-	// Crop the image around the edges
+	// Crop the image around the edges to fit an exact number of pieces
 	croppedImage := imaging.CropCenter(src, newx, newy)
-	// fmt.Printf("%T\n", croppedImage.At(600, 400))
-	// s, q, f, k := croppedImage.At(600, 400).RGBA()
-	// fmt.Printf("%v\t%v\t%v\t%v\n", (s)>>8, (q)>>8, (f)>>8, k)
 
 	numPiecesX := newx / pieceWidth
 	numPiecesY := newy / pieceHeight
 	// totalPieces := numPiecesX * numPiecesY
 
+	// 2D slice to hold the pieces
 	var pieces [][]piece
+
+	// The origin of each piece relative to the source image
 	originx, originy := 0, 0
+
+	// Populate the pieces slice
 	for j := 0; j < numPiecesY; j++ {
-		originy = j*pieceHeight + 1
+		originy = j * pieceHeight //+ 1
 
 		var row []piece
 		for i := 0; i < numPiecesX; i++ {
-			originx = i*pieceWidth + 1
+			originx = i * pieceWidth //+ 1
 			p := piece{origin: [2]int{originx, originy}}
 			row = append(row, p)
 		}
 		pieces = append(pieces, row)
 	}
-	// width := 1200
-	// height := 650
+
+	// Output image created now for debugging purposes
 	orig := image.Point{0, 0}
 	end := image.Point{newx, newy}
-	// cyan := color.RGBA{100, 200, 200, 0xff}
-
 	newImage := image.NewRGBA(image.Rectangle{orig, end})
-	// fmt.Println(pieces)
+
+	// Debug
+	yellow := color.RGBA{255, 255, 0, 0xff}
+	_ = yellow
+
 	// For each piece
 	for u, m := range pieces {
 		for v, n := range m {
 			var sum [3]uint32
+
+			// newImage.Set(n.origin[0], n.origin[1], yellow)
 
 			// For each pixel within that piece
 			for i := n.origin[0]; i < n.origin[0]+pieceWidth; i++ {
@@ -72,18 +80,24 @@ func main() {
 					// if (r + g + b) != 0 {
 					// 	fmt.Println(r>>8, g>>8, b>>8)
 					// }
-					sum[0] += (r)
-					sum[1] += (g)
-					sum[2] += (b)
+					sum[0] += r
+					sum[1] += g
+					sum[2] += b
 					// newImage.Set(i, j, croppedImage.At(i, j))
+					// if i == n.origin[0] || j == n.origin[1] {
+					// 	newImage.Set(i, j, yellow)
+					// }
 				}
 			}
+			// *** Average over the piece
 			sum[0] = sum[0] / (pieceWidth * pieceHeight)
 			sum[1] = sum[1] / (pieceWidth * pieceHeight)
 			sum[2] = sum[2] / (pieceWidth * pieceHeight)
 			pieces[u][v].averageColour = sum
-			for i := n.origin[0]; i < n.origin[0]+50; i++ {
-				for j := n.origin[1]; j < n.origin[1]+50; j++ {
+
+			// Set colours in output image  here for debugging
+			for i := n.origin[0]; i < n.origin[0]+pieceWidth; i++ {
+				for j := n.origin[1]; j < n.origin[1]+pieceHeight; j++ {
 					newImage.Set(i, j, color.RGBA{uint8(sum[0]), uint8(sum[1]), uint8(sum[2]), 0xff})
 				}
 			}
@@ -96,7 +110,7 @@ func main() {
 	// 	}
 	// }
 
-	saveImage(newImage, "images/comeon.jpg")
+	saveImage(newImage, "images/output.jpg")
 }
 
 func saveImage(i *image.RGBA, p string) {
@@ -106,7 +120,4 @@ func saveImage(i *image.RGBA, p string) {
 	if err != nil {
 		panic(err.Error())
 	}
-	// func newPiece() *piece {
-	// 	return &piece{width: pieceWidth, height: pieceHeight}
-	// }
 }
